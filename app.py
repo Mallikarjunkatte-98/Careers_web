70d5a0eff39e65fbb4162b4ad60451e3241447f5
Your NameRavi <mallikarjunkatte98@gmail.com>
2025-04-11 14:01:12 +0400

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_():
-    return "Hello world"
+    return render_template("home.html")
 
 
 
